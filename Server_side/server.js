838090aed9d0eb83aa1474f034c2869454800965
Adham Khalilieh <Adham1011@github.com>
2018-09-03 11:38:58 +0300

    allow cors

--- Server_side/server.js
+++ Server_side/server.js
@@ -11,8 +11,9 @@ app.set('port',port);
 
 app.use((req,res,next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type,Accept");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type,Accept, Authorization");
+    res.header("Access-Control-Allow-Methods",'GET,POST,DELETE,OPTIONS');
+    res.set("Content-Type", "application/json");
     next();
 });
 
